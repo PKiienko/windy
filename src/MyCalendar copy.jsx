29f8d32089c458c26cf0ac79fe08d6a9975f7cff
PKiienko <petro.kiienko@gmail.com>
2022-12-16 14:58:06 +0200

    keeper ok

--- src/MyCalendar copy.jsx
+++ src/MyCalendar copy.jsx
@@ -1,50 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import * as xlsx from 'xlsx';
-import Calendar from 'react-calendar';
 import './MyCalendar.css';
 
 const MyCalendar = ({ showMyCalendar, onMyCalendar }) => {
   const [fileData, setFileData] = useState(null);
   const [keeper, setKeeper] = useState('Lets findout');
-  const [date, setDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState({
+    day: 13,
+    month: 11,
+    year: 2022,
+  });
 
-  useEffect(() => {
+  useEffect(() => {}, [selectedDate]);
+
+  //setting date according to date picker
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setSelectedDate({
+      ...selectedDate,
+      [name]: +value,
+    });
+    console.log(typeof selectedDate.day);
+
     keeperFinder();
-  }, [date, fileData]);
-
-  //select date on the calendar
-  const dateSelecting = () => {
-    console.log('Date is', date);
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    console.log(day, month, year);
   };
 
   //finding kepper on selected date
   const keeperFinder = () => {
-    dateSelecting();
     if (fileData === null) {
-      // setKeeper('Select file');
       console.log('file not selected');
     }
-    console.log(date);
-
-    if (fileData) {
-      for (let i = 0; i < fileData.length; i++) {
-        if (
-          fileData[i].day === date.getDate() &&
-          fileData[i].month === date.getMonth() + 1 &&
-          fileData[i].year === date.getFullYear()
-        ) {
-          setKeeper(fileData[i].keeper);
-          console.log(fileData);
-          console.log('fileData is', fileData[i]);
-          console.log('selectedDate is', date);
-          console.log('FileData Kepper is', fileData[i].keeper);
-          console.log('Setted Kepper is', keeper);
-        }
+    for (let i = 0; i < fileData.length; i++) {
+      if (
+        fileData[i].day === selectedDate.day &&
+        fileData[i].month === selectedDate.month &&
+        fileData[i].year === selectedDate.year
+      ) {
+        console.log(fileData);
+        console.log('fileData is', fileData[i]);
+        console.log('selectedDate is', selectedDate);
+        setKeeper(fileData[i].keeper);
+        console.log('FileData Kepper is', fileData[i].keeper);
+        console.log('Setted Kepper is', keeper);
       }
     }
   };
@@ -74,11 +73,35 @@ const MyCalendar = ({ showMyCalendar, onMyCalendar }) => {
             <AiOutlineClose onClick={onMyCalendar} />
           </div>
 
-          <Calendar
-            onChange={setDate}
-            value={date}
-            className={['c1', 'c2']}
-          />
+          <div className='date-picker'>
+            <input
+              className='date-unit'
+              type='number'
+              min='1'
+              max='31'
+              value={selectedDate.day}
+              name='day'
+              onChange={handleInputChange}
+            />
+            <input
+              className='date-unit'
+              type='number'
+              min='1'
+              max='12'
+              value={selectedDate.month}
+              name='month'
+              onChange={handleInputChange}
+            />
+            <input
+              className='date-unit'
+              type='number'
+              min='2020'
+              max='2030'
+              value={selectedDate.year}
+              name='year'
+              onChange={handleInputChange}
+            />
+          </div>
 
           <div className='keeper-name'>
             <h4>Keeper:</h4>
